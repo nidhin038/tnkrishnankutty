9cc39b99770deb4ac8aad3edd821078114580d9e
Krishna Gadha <krishnagaadhamh@gmail.com>
2019-07-19 23:18:58 +0530

    life events changes

--- filmography.php
+++ filmography.php
@@ -23,29 +23,43 @@
 <script>
 
 $( document ).ready(function() {
-$.get("json/films.json", function(data, status){
-    console.log(data);
-	var films = "";
-	var newFilm = "";
-	for(var i=0;i<data.length;i++){
-		newFilm = '<div class="col-md-4">'+
-	              '<div class="card mb-4 box-shadow">'+
-	                '<img class="card-img-top" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="'+data[i].imgUrl+'" data-holder-rendered="true">'+
-	                '<div class="card-body">'+
-	                  '<p class="card-text">'+data[i].desc+'</p>'+
-	                  '<div class="d-flex justify-content-between align-items-center">'+
-	                    '<div class="btn-group">'+
-	                      
-	                    '</div>'+
-	                    '<a href=""><small class="text-muted">More Details</small></a>'+
-	                  '</div>'+
-	                '</div>'+
-	              '</div>'+
-	'</div>';
-		films = films + newFilm;	
-	}
-	$('#films-container').html(films);
-  });
+  
+      $.ajax({
+      type: "GET",
+      url: "json/films.json",
+      success: function (data, status) {
+        var films = "";
+		var newFilm = "";
+		for(var i=0;i<data.length;i++){
+			newFilm = '<div class="col-md-4">'+
+					  '<div class="card mb-4 box-shadow">'+
+						'<img class="card-img-top" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="'+data[i].imgUrl+'" data-holder-rendered="true">'+
+						'<div class="card-body">'+
+						  '<p class="card-text">'+data[i].desc+'</p>'+
+						  '<div class="d-flex justify-content-between align-items-center">'+
+							'<div class="btn-group">'+
+							  
+							'</div>'+
+							'<a href="'+data[i].link+'"><small class="text-muted">More Details</small></a>'+
+						  '</div>'+
+						'</div>'+
+					  '</div>'+
+		'</div>';
+			films = films + newFilm;	
+		}
+		$('#films-container').html(films);
+      },
+      error: function () {
+        alert('Something went wrong. Please reload the page.')
+      },
+      complete: function () {
+
+      }
+    });
+  
+  
+  
+  
   
 });
 
--- lifestory.php
+++ lifestory.php
@@ -15,7 +15,7 @@
 	    <div id="timeline">
 			<div class="row timeline-movement timeline-movement-top">
 				<div class="timeline-badge timeline-future-movement">
-						<p>2018</p>
+						<p>1952</p>
 				</div>
 			</div>
 			<div class="row timeline-movement">
@@ -31,10 +31,10 @@
 										<li class="img-wraping"><a href="#"><img src="http://via.placeholder.com/250/000000" class="img-responsive" alt="user-image" /></a></li>
 									</div>
 									<div class="righting-wrap">
-										<li><a href="#" class="importo">Mussum ipsum cacilds</a></li>
-										<li><span class="causale" style="color:#000; font-weight: 600;">Developer </span> </li>
-										<li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
-										<li><p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 13/01/2018, 13:05"</small></p> </li>
+										<li><a href="#" class="importo">JOINED UDAYA</a></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;"> Ready to do any job but liked camera</span> </li>
+										<li><span class="causale lifeevent-desc" > Kunchacko gave the post of camera assistant</span> </li>
+										<li><p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 13/01/1952</small></p> </li>
 									</div>
 									<div class="clear"></div>
 								</ul>
@@ -52,15 +52,15 @@
 				<div class="offset-sm-6 col-sm-6  timeline-item">
 					<div class="row">
 						<div class="offset-sm-1 col-sm-11">
-							<div class="timeline-panel debits  anim animate  fadeInRight">
+							<div class="timeline-panel debits  anim animate  fadeInRight timeline-height">
 								<ul class="timeline-panel-ul">
 									<div class="lefting-wrap">
 										<li class="img-wraping"><a href="#"><img src="http://via.placeholder.com/250/000000" class="img-responsive" alt="user-image" /></a></li>
 									</div>
 									<div class="righting-wrap">
-										<li><a href="#" class="importo">Mussum ipsum cacilds</a></li>
-										<li><span class="causale" style="color:#000; font-weight: 600;">Web Designer </span> </li>
-										<li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
+										<li><a href="#" class="importo">Became the Part and Parcel</a></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Camera Assistant to Allrounder </span> </li>
+										<li><span class="causale lifeevent-desc">Became a Trustworthy worker</span> </li>
 										<li><p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 12/01/2018, 13:05"</small></p> </li>
 									</div>
 									<div class="clear"></div>
@@ -87,7 +87,7 @@
 									<div class="righting-wrap">
 										<li><a href="#" class="importo">Mussum ipsum cacilds</a></li>
 										<li><span class="causale" style="color:#000; font-weight: 600;">Engineer </span> </li>
-										<li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
+										<li><span class="causale lifeevent-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
 										<li><p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11/01/2018, 13:05"</small></p> </li>
 									</div>
 									<div class="clear"></div>
@@ -101,7 +101,7 @@
 			
 			<div class="row timeline-movement timeline-movement-top">
 				<div class="timeline-badge timeline-future-movement">
-						<p>2017</p>
+						<p>1960</p>
 				</div>
 			</div>
 			
@@ -113,15 +113,15 @@
 				<div class="offset-sm-6 col-sm-6  timeline-item">
 					<div class="row">
 						<div class="offset-sm-1 col-sm-11">
-							<div class="timeline-panel debits  anim animate  fadeInRight">
+							<div class="timeline-panel debits  anim animate  fadeInRight timeline-height">
 								<ul class="timeline-panel-ul">
 									<div class="lefting-wrap">
 										<li class="img-wraping"><a href="#"><img src="http://via.placeholder.com/250/000000" class="img-responsive" alt="user-image" /></a></li>
 									</div>
-									<div class="righting-wrap">
-										<li><a href="#" class="importo">Mussum ipsum cacilds</a></li>
-										<li><span class="causale" style="color:#000; font-weight: 600;">Web Designer </span> </li>
-										<li><span class="causale">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span> </li>
+									<div class="righting-wrap"> 
+										<li><a href="#" class="importo">Independant Cinematographer</a></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">First film Umma</span> </li>
+										<li><span class="causale lifeevent-desc">Umma paved the way for the historical career of T N Krishnankutty and the hit maker Kunchacko along with the glorious rise of Udaya Studio. </span> </li>
 										<li><p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 12/01/2018, 13:05"</small></p> </li>
 									</div>
 									<div class="clear"></div>
