675465fe8418e873969008c8b13d522880b192e9
Nidhin N <nidhin038@gmail.com>
2019-08-18 20:48:57 +0530

    Awards page changes

--- awards.php
+++ awards.php
@@ -11,5 +11,39 @@
 <div class="page-title">
 	<h4>Honours</h4>
 </div>
+<div class="container">
+<div class="row honour-row border-bottom">
+	<div class="col-md-2 honour border-right no-border-mobile">
+		<img src="images/IMG_3536.jpg">
+	</div>
+	<div class="col-md-10 margin-top-mobile">
+		<h6>Asianet</h6>
+		<div>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </div>
+	</div>
+	<hr>
+</div>
 
+<div class="row honour-row border-bottom">
+	<div class="col-md-2 col-sm-2 honour border-right no-border-mobile">
+		<img src="images/IMG_3536.jpg">
+	</div>
+	<div class="col-md-10 col-sm-10 margin-top-mobile">
+		<h6>Asianet</h6>
+		<div>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </div>
+	</div>
+	<hr>
+</div>
+
+<div class="row honour-row border-bottom">
+	<div class="col-md-2 honour border-right no-border-mobile">
+		<img src="images/IMG_3536.jpg">
+	</div>
+	<div class="col-md-10 margin-top-mobile">
+		<h6>Asianet</h6>
+		<div>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </div>
+	</div>
+	<hr>
+</div>
+
+</div>
 </div>
